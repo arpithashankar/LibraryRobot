6b191bdc67b2ef84288b5a6743aa50e796efaf1b
Arpitha Shankar S I <arpithashankar@github.com>
2020-07-07 15:13:09 +0530

    LibraryRobot

---
+++ BookAssignment/BookAssignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bookLib list = new bookLib();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1 to Display");
+                Console.WriteLine("2 to Add book");
+                Console.WriteLine("3 to Delete book");
+                Console.WriteLine("4 to Search a book");
+                Console.WriteLine("5 to Exit");
+                Console.WriteLine("Enter your choice");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        list.Display();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Author name");
+                        string AuthorName = Console.ReadLine();
+                        Console.WriteLine("Enter the Book name");
+                        string BookName = Console.ReadLine();
+                        Console.WriteLine("Enter the ISBN");
+                        string ISBN = Console.ReadLine();
+                        string FullText = (AuthorName + "," + BookName + "," + ISBN);
+                        list.AddBook(FullText);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter the name of the book to be deleted ");
+                        string BookN = Console.ReadLine();
+                        list.DelBook(BookN);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the ISBN to search");
+                        string ISBNno = Console.ReadLine();
+                        list.Search(ISBNno);
+                        break;
+                    case 5:
+                        Console.WriteLine("Thank you");
+                        break;
+                    default:
+                        Console.WriteLine("Please enter the right choice");
+                        break;
+                    
+                }
+
+            }
+        }
+    }
+}
---
+++ BookAssignment/BookAssignment/bookLib.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BookAssignment
+{
+    class bookLib
+    {
+        // to display book details
+        public void Display()
+        {
+            Console.WriteLine("The Library book details");
+            string[] lines = File.ReadAllLines(@"E:\learning\book.txt");
+
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+        
+        // to add book details
+
+        public void AddBook(string Data)
+        {
+            StreamWriter sw = File.AppendText(@"E:\learning\book.txt");
+            sw.WriteLine(Data);
+            sw.Close();
+
+        }
+
+        //to delete book
+        public void DelBook(string BookName)
+        {
+            string strOldText;
+            string n = "";
+            StreamReader sr = File.OpenText(@"E:\learning\book.txt");
+            while ((strOldText = sr.ReadLine()) != null)
+            {
+                if (!strOldText.Contains(BookName))
+                {
+                    n += strOldText + Environment.NewLine;
+                }
+            }
+            sr.Close();
+            File.WriteAllText(@"E:\learning\book.txt", n);
+        }
+
+       //to search a book 
+        public void Search(string ISBNnumber)
+        {
+            StreamReader reader = new StreamReader(@"E:\learning\book.txt");
+            string record = reader.ReadLine();
+            while (record != null)
+            {
+                if (record.Contains(ISBNnumber))
+                {
+
+                    Console.WriteLine("The details of the book as follows \n" + record);
+                }
+                
+                record = reader.ReadLine();
+            }
+            reader.Close();
+            
+        }
+    }
+}
